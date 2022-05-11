f55bcc9778e8b49cc2b07b24ff4b92003c0e5806
patti8 <itupatti@gmail.com>
2022-05-11 16:36:56 +0800

    updated logo

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -43,7 +43,7 @@ module Receipts
       end
     end
 
-    def header(company: {}, height: 70)
+    def header(company: {}, height: 200)
       logo = company[:logo]
 
       if logo.nil?
--- lib/receipts/register.rb
+++ lib/receipts/register.rb
@@ -1,9 +1,5 @@
 module Receipts
   class Register < Base
     @title = "Bukti Pendaftaran"
-
-    def header(company: {}, height: 200)
-    end
-
   end
 end
