cbba1d098328d7d8afb6e80f8850f83b5d9930f1
patti8 <itupatti@gmail.com>
2022-05-11 16:51:47 +0800

    adding banner

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -45,12 +45,13 @@ module Receipts
 
     def header(company: {}, height: 80)
       logo = company[:logo]
+      banner = company[:banner]
 
       if logo.nil?
         text company.fetch(:name), align: :right, style: :bold, size: 16, color: "4b5563"
       else
         image load_image(logo), height: height, margin_bottom: 15, position: :left
-        image load_image(logo), height: height, margin_bottom: 15, position: :right
+        image load_image(banner), height: height, margin_bottom: 15, position: :right
       end
 
       move_up height
